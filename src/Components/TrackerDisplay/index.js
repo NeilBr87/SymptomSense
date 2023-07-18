5f4bd39ffe1c346a9ca07f261606df2d723703da
NeilBrooks <neil.brooks87@gmail.com>
2023-07-18 16:23:44 +0100

    Displayed card based on array, now need to add extra entries

--- src/Components/TrackerDisplay/index.js
+++ src/Components/TrackerDisplay/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Typography, Checkbox } from '@material-ui/core';
+
 export default function TrackerDisplay(props) {
 
-    return (    
-        <div style={{border: '1px solid black', padding: '15px', display: 'flex', flexDirection: 'column'}}>
-            <Typography>Day: {props.day}</Typography>
-            <Typography>Symptom: {props.symptomName} </Typography>
-        </div>
-    )
-
+  return (
+    <div>
+      <div style={{ border: '1px solid black', padding: '15px', display: 'flex', flexDirection: 'column' }}>
+        <Typography>Day {props.day}</Typography>
+        {props.entries.map((entry, index) => (
+          <div key={index}>
+            <Typography>Symptom Name: {entry.symptomName}</Typography>
+            <Typography>Description: {entry.symptomDescription}</Typography>
+            <Typography>Occurred today? {entry.symptomOccurred ? 'Yes' : 'No'}</Typography>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/Components/TrackerForm/index.js
+++ src/Components/TrackerForm/index.js
@@ -1,52 +1,58 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField, Typography, Checkbox } from '@material-ui/core';
 import TrackerDisplay from '../TrackerDisplay';
 
 export default function TrackerForm(props) {
+  const [entry, setEntry] = useState({
+    symptomName: '',
+    symptomDescription: '',
+    symptomOccurred: false,
+    symptomSeverity: '',
+  });
+  const [submitted, setSubmitted] = useState(false);
+  const [entries, setEntries] = useState([]);
 
-    const [symptomName, setSymptomName] = useState('');
-    const [symptomDescription, setSymptomDescription] = useState('');
-    const [symptomOccurred, setSymptomOccurred] = useState(false);
-    const [symptomSeverity, setSymptomSeverity] = useState('');
-    const [submitted, setSubmitted] = useState(false);
+  function handleSymptomNameChange(event) {
+    setEntry({ ...entry, symptomName: event.target.value });
+  }
 
-    function handleSymptomNameChange(event) {
-        setSymptomName(event.target.value);
-    }
+  function handleSymptomDescriptionChange(event) {
+    setEntry({ ...entry, symptomDescription: event.target.value });
+  }
 
-    function handleSymptomDescriptionChange(event) {
-        setSymptomDescription(event.target.value);
-    }
+  function handleSymptomOccurredChange(event) {
+    setEntry({ ...entry, symptomOccurred: event.target.checked });
+  }
 
-    function handleSymptomOccurredChange(event) {
-        setSymptomOccurred(event.target.value);
-    }
+  function handleSubmit() {
+    setSubmitted(true);
+    setEntries([...entries, entry]);
+    setEntry({
+      symptomName: '',
+      symptomDescription: '',
+      symptomOccurred: false,
+      symptomSeverity: '',
+    });
+  }
 
-    //Now the submit
-    function handleSubmit() {
-        setSubmitted(true);
-        console.log('submitted');   
-    }
-
-    return (
-        <div>
-            {!submitted &&
-                <div style={{border: '1px solid black', padding: '15px', display: 'flex', flexDirection: 'column'}}>
-                    <Typography>Day {props.day}</Typography>
-                    <TextField onChange={handleSymptomNameChange} id="outlined-basic" label="Symptom name" variant="outlined" />
-                    <TextField onChange={handleSymptomDescriptionChange} id="outlined-basic" label="Description" variant="outlined" />
-                    <Typography>Occurred today?</Typography>
-                    <Checkbox onChange={handleSymptomOccurredChange} id="outlined-basic" label="Occurred" variant="outlined" />
-                    <button onClick={handleSubmit} style={{width: '100px', height: '50px', backgroundColor: 'rgb(226, 80, 65)', color: 'white', border: 'none', borderRadius: '10px', marginTop: '10px'}}>Submit</button>
-                </div>}
-            
-
-            {submitted && 
-                <div>
-                    <TrackerDisplay day={props.day} symptomName={symptomName} />
-                </div> }
-
+  return (
+    <div>
+      {!submitted && (
+        <div style={{ border: '1px solid black', padding: '15px', display: 'flex', flexDirection: 'column' }}>
+          <Typography>Day {props.day}</Typography>
+          <TextField onChange={handleSymptomNameChange} value={entry.symptomName} id="outlined-basic" label="Symptom name" variant="outlined" />
+          <TextField onChange={handleSymptomDescriptionChange} value={entry.symptomDescription} id="outlined-basic" label="Description" variant="outlined" />
+          <Typography>Occurred today?</Typography>
+          <Checkbox onChange={handleSymptomOccurredChange} checked={entry.symptomOccurred} id="outlined-basic" label="Occurred" variant="outlined" />
+          <button onClick={handleSubmit} style={{ width: '100px', height: '50px', backgroundColor: 'rgb(226, 80, 65)', color: 'white', border: 'none', borderRadius: '10px', marginTop: '10px' }}>Submit</button>
         </div>
-    );
+      )}
+
+      {submitted && (
+        <div>
+          <TrackerDisplay day={props.day} entries={entries} />
+        </div>
+      )}
+    </div>
+  );
 }
